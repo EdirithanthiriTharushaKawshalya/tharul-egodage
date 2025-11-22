487daf1c8f47b944f6d57cead684397d7d81804c
Tharusha Kawshalya <tharusha.k.dev@gmail.com>
2025-11-22 18:40:07 +0530

    image

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,33 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "plus.unsplash.com", 
       },
+      // Allow Facebook Images
+      {
+        protocol: "https",
+        hostname: "scontent.xx.fbcdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "scontent-*.fbcdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "*.fbcdn.net",
+      },
+      // Allow Google Drive & Google Content
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      // Allow Firebase Storage (Highly Recommended for your stack)
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      }
     ],
   },
 };
